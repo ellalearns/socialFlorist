eeae3eb559ca24df2f096123ada3ce2264929930
ellalearns <ellalearnscopy@gmail.com>
2025-07-15 12:26:21 +0100

    add profile picture

--- app/(pages)/index.tsx
+++ app/(pages)/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, View, SafeAreaView } from "react-native"
+import { ImageBackground, View, SafeAreaView, Image, Text, TouchableOpacity } from "react-native"
 import images from "@/constants/images"
 import GradientBackground from "@/components/GradientBackground"
 import Header from "@/components/Header"
@@ -7,17 +7,55 @@ export default function Index() {
   return (
     <GradientBackground>
       <View
-      style={{
-        padding: 24,
-        height: "100%",
-        width: "100%",
-        marginTop: 36,
-        flex: 1
-        // zIndex: 100
-        // backgroundColor: "yellow"
-      }}
+        style={{
+          padding: 24,
+          height: "100%",
+          width: "100%",
+          marginTop: 36,
+          flex: 1,
+          justifyContent: "space-evenly"
+        }}
       >
         <Header />
+        <View style={{
+          // backgroundColor: "grey",
+          height: "30%",
+          width: "70%"
+        }}>
+          <View style={{
+            borderRadius: 50
+          }}>
+            <Image source={images.pic1} style={{
+              height: "100%",
+              width: "100%",
+              borderRadius: 300
+            }}>
+            </Image>
+          </View>
+          <View style={{
+            height: 70,
+            width: 70,
+            backgroundColor: "#67bc5f",
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            borderRadius: 50,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+            <TouchableOpacity style={{
+              height: "100%",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Image source={images.chat} style={{
+                height: "50%",
+                width: "50%"
+              }} />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </GradientBackground>
   )
--- constants/images.ts
+++ constants/images.ts
@@ -2,7 +2,11 @@ const images = {
     one: require("../assets/images/1.png"),
     gradient: require("../assets/images/gradient.png"),
     back: require("@/assets/images/back-button.png"),
-    menu: require("@/assets/images/menu-button.png")
+    menu: require("@/assets/images/menu-button.png"),
+    user: require("@/assets/images/user.png"),
+    mainPic: require("@/assets/images/mainPic.png"),
+    pic1: require("@/assets/images/pic1.png"),
+    chat: require("@/assets/images/messenger.png")
 }
 
 export default images
